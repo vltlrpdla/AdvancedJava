c52efa861bc6493a1c9e35b9e511649660389d90
vltlrpdla@hanmail.net <vltlrpdla@hanmail.net>
2018-06-07 23:05:43 +0900

    updated template method pattern
    hook, abstract

---
+++ src/com/advancedjava/designpattern/template/Warrior.java
@@ -0,0 +1,14 @@
+package com.advancedjava.designpattern.template;
+
+public class Warrior extends Person {
+
+	@Override
+	void prepareWeapon() {
+		System.out.println("검을 닦습니다.");
+	}
+
+	@Override
+	void prepareArmor() {
+		System.out.println("갑옷을 입습니다.");
+	}
+}
